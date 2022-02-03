e08c0068af021204da18fab02b579076f73b900c
ALBERT <thatguyintech@gmail.com>
2022-02-03 10:52:18 -0800

    create fetch script

---
+++ fetch-script.js
@@ -0,0 +1,22 @@
+// alchemy-nft-api/fetch-script.js
+import fetch from 'node-fetch';
+
+// Setup request options:
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+// Replace with your Alchemy API key:
+const apiKey = "demo";
+const baseURL = `https://eth-mainnet.g.alchemy.com/${apiKey}/v1/getNFTs/`;
+// Replace with the wallet address you want to query:
+const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+const fetchURL = `${baseURL}?owner=${ownerAddr}`;
+
+// Make the request and print the formatted response:
+fetch(fetchURL, requestOptions)
+  .then(response => response.json())
+  .then(response => JSON.stringify(response, null, 2))
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
