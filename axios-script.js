30ae4f7f441b21a8b080caac66986d396597415f
ALBERT <thatguyintech@gmail.com>
2022-02-03 10:55:20 -0800

    create axios script

---
+++ axios-script.js
@@ -0,0 +1,19 @@
+// alchemy-nft-api/axios-script.js
+import axios from 'axios';
+
+// Replace with your Alchemy API key:
+const apiKey = "demo";
+const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${apiKey}/getNFTs/`;
+// Replace with the wallet address you want to query for NFTs:
+const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+
+// Construct the axios request:
+var config = {
+  method: 'get',
+  url: `${baseURL}?owner=${ownerAddr}`
+};
+
+// Make the request and print the formatted response:
+axios(config)
+.then(response => console.log(JSON.stringify(response.data, null, 2)))
+.catch(error => console.log(error));
